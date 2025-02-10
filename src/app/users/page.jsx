2e5feec1fb44ca91189956f6e0f60d9bac54c2da
Aditya201514049 <adityasingha084@gmail.com>
2025-02-11 01:19:44 +0600

    Add user page and integrate Firebase with Tailwind CSS and DaisyUI

---
+++ src/app/users/page.jsx
@@ -0,0 +1,10 @@
+// src/app/user/page.jsx
+export default function UserPage() {
+    return (
+      <div>
+        <h1>User Page</h1>
+        <p>Welcome to the user page!</p>
+      </div>
+    );
+  }
+  
